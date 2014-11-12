ddac6bab1b03439cf9958b9ee3afe4875eb61963
袁小燕 <yuanxiaoyan@192.168.2.107>
2014-11-12 21:50:58 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 袁小燕 on 14-11-12.
+//  Copyright (c) 2014年 spark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 袁小燕 on 14-11-12.
+//  Copyright (c) 2014年 spark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
